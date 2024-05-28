744bd7e5d7333adbd9e363dcdb420a3a34f1aa7f
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-05-27 23:19:27 -0400

    Inital Setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+
+export default function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/About" element={<About />}></Route>
+				<Route path="/SignUp" element={<SignUp />}></Route>
+				<Route path="/SignIn" element={<SignIn />}></Route>
+				<Route path="/Profile" element={<Profile />}></Route>
+			</Routes>
+		</BrowserRouter>
+	);
+}
