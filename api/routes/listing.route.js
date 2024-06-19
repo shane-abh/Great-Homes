f5d5c47231f6603f1e8946e8b650fe3b3fb61e5b
Shane <shaneabh777@gmail.com>
2024-06-19 16:25:26 -0400

    Fixed validation error

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -11,21 +11,7 @@ const stripTags = (input) => {
     return tempDiv.textContent || tempDiv.innerText || "";
   };
 
-router.post('/create', verifyToken,  [
-    body('name').notEmpty().withMessage('Name is required').customSanitizer(stripTags),
-    body('description').notEmpty().withMessage('Description is required').customSanitizer(stripTags),
-    body('address').notEmpty().withMessage('Address is required').customSanitizer(stripTags),
-    body('regularPrice').isNumeric().withMessage('Regular price must be a number'),
-    body('discountPrice').isNumeric().withMessage('Discount price must be a number'),
-    body('bathrooms').isInt({ min: 0 }).withMessage('Bathrooms must be a non-negative integer'),
-    body('bedrooms').isInt({ min: 0 }).withMessage('Bedrooms must be a non-negative integer'),
-    body('furnished').isBoolean().withMessage('Furnished must be a boolean value'),
-    body('parking').isBoolean().withMessage('Parking must be a boolean value'),
-    body('type').isIn(['sale', 'rent']).withMessage('Type must be either "sale" or "rent"'),
-    body('offer').isBoolean().withMessage('Offer must be a boolean value'),
-    body('imageUrls').isArray({ min: 1 }).withMessage('Image URLs must be an array with at least one URL'),
-    body('userRef').notEmpty().withMessage('User reference is required').customSanitizer(stripTags),
-  ], createListing);
+router.post('/create', verifyToken,   createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
 router.get('/get/:id', getListing);
