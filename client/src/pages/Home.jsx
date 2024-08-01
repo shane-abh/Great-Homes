cdb61427e9aeec590fdf12191a05c5a965995ac1
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-08-01 10:24:15 -0400

    Hero section make responsive

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -63,8 +63,9 @@ const Home = () => {
 					backgroundColor: "black",
 				}}
 			>
-				<div className=" flex flex-col gap-6 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-6xl mx-auto z-10 text-gray-600 bg-black/0">
-					<h1 className="text-4xl lg:text-6xl text-center font-cursiveFont ">
+				<div className="absolute inset-0 bg-black opacity-50 rounded-sm"></div>
+				<div className="w-full px-2 flex flex-col gap-6 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-6xl mx-auto z-10 text-gray-600 bg-black/0">
+					<h1 className="text-4xl text-white lg:text-6xl text-center font-cursiveFont">
 						Find your dream
 						<br />
 						Here
